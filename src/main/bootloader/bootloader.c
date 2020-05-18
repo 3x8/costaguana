ec84ed577fc8d851bc5e80c40564c61322d4edd0
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-18 18:35:03 +0200

    cleanup

--- src/main/bootloader/bootloader.c
+++ src/main/bootloader/bootloader.c
@@ -1,15 +1,15 @@
 #include "bootloader.h"
 
-void bootloaderFlashWrite(uint8_t *data, int length, uint32_t add) {
-  uint16_t data_to_FLASH[length / 2];
-  memset(data_to_FLASH, 0, length / 2);
+void bootloaderFlashWrite(uint8_t *data, int length, uint32_t address) {
+  uint16_t dataFlashBuffer[length / 2];
+  memset(dataFlashBuffer, 0, length / 2);
 
   for (int i = 0; i < length / 2 ; i ++ ) {
-    data_to_FLASH[i] =  data[i*2+1] << 8 | data[i*2];   // make 16 bit
+    dataFlashBuffer[i] =  data[i*2+1] << 8 | data[i*2];   // make 16 bit
   }
 
   // unlock flash
-  volatile uint32_t data_length = length / 2;
+  volatile uint32_t dataFlashLength = length / 2;
   while ((FLASH->SR & FLASH_SR_BSY) != 0) {
     // wait
   }
@@ -20,9 +20,9 @@ void bootloaderFlashWrite(uint8_t *data, int length, uint32_t add) {
   }
 
   // erase page if address even divisable by 1024
-  if ((add % 1024) == 0){
+  if ((address % 1024) == 0){
     FLASH->CR |= FLASH_CR_PER;
-    FLASH->AR = add;
+    FLASH->AR = address;
     FLASH->CR |= FLASH_CR_STRT;
     while ((FLASH->SR & FLASH_SR_BSY) != 0){
       // wait
@@ -35,10 +35,10 @@ void bootloaderFlashWrite(uint8_t *data, int length, uint32_t add) {
     FLASH->CR &= ~FLASH_CR_PER;
   }
 
-  volatile uint32_t write_cnt=0, index=0;
-  while (index < data_length) {
+  volatile uint32_t writeCounter = 0, index = 0;
+  while (index < dataFlashLength) {
     FLASH->CR |= FLASH_CR_PG;
-    *(__IO uint16_t*)(add+write_cnt) = data_to_FLASH[index];
+    *(__IO uint16_t*)(address+writeCounter) = dataFlashBuffer[index];
     while ((FLASH->SR & FLASH_SR_BSY) != 0) {
       // wait
     }
@@ -48,14 +48,14 @@ void bootloaderFlashWrite(uint8_t *data, int length, uint32_t add) {
     // error
     }
     FLASH->CR &= ~FLASH_CR_PG;
-    write_cnt += 2;
+    writeCounter += 2;
     index++;
   }
   SET_BIT(FLASH->CR, FLASH_CR_LOCK);
 }
 
-void bootloaderFlashRead(uint8_t*  data , uint32_t add , int out_buff_len){;
+void bootloaderFlashRead(uint8_t*  data , uint32_t address , int out_buff_len){;
   for (int i = 0; i < out_buff_len ; i ++) {
-    data[i] = *(uint8_t*)(add + i);
+    data[i] = *(uint8_t*)(address + i);
   }
 }
