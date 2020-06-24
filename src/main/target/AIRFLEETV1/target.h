9a375430cf7245a65c0161612da9ce9d1b89426c
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-06-24 09:22:48 +0200

    io

--- src/main/target/AIRFLEETV1/target.h
+++ src/main/target/AIRFLEETV1/target.h
@@ -2,13 +2,13 @@
 
 // input begin
 #define INPUT_GPIO      GPIOA
-#define INPUT_PIN       LL_GPIO_PIN_3
+#define INPUT_PIN       LL_GPIO_PIN_6
 
 // LEDs
 #define LED_RED_GPIO    GPIOA
-#define LED_RED_PIN     LL_GPIO_PIN_15
+#define LED_RED_PIN     LL_GPIO_PIN_3
 #define LED_GREEN_GPIO  GPIOA
-#define LED_GREEN_PIN   LL_GPIO_PIN_15
+#define LED_GREEN_PIN   LL_GPIO_PIN_3
 #define LED_BLUE_GPIO   GPIOA
-#define LED_BLUE_PIN    LL_GPIO_PIN_15
+#define LED_BLUE_PIN    LL_GPIO_PIN_3
 #define LED_MASK        LL_AHB1_GRP1_PERIPH_GPIOA | LL_AHB1_GRP1_PERIPH_GPIOB
