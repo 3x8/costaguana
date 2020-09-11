ef35a2d293445bb1765f77b413b3cb1adccb113e
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-09-11 09:54:22 +0200

    bump version

--- src/main/version/version.h
+++ src/main/version/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define FW_FIRMWARE_NAME    "costaguana"
-#define FW_VERSION_MAJOR    0
-#define FW_VERSION_MINOR    3
-#define FW_VERSION_PATCH    3
+#define FW_VERSION_MAJOR    1
+#define FW_VERSION_MINOR    0
+#define FW_VERSION_PATCH    1
