1b5d2b42c06614c8778edd3912e56a17dff79d68
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-10-07 09:49:24 +0200

    RAZOR32V2

--- README.md
+++ README.md
@@ -7,9 +7,16 @@
   * serial 1wire passthrough 4way interface
 
 **supported hardware:**
+  * RAZOR32V2  *(new)*
   * WRAITH32V2  *(official)*
   * TYPHOON32V2  *(official)*
   * WRAITH32
+  * WRAITH32MINI
+  * SUCCEX50AV2
+  * SUCCEXMINI40A
+  * DYS35ARIA
+  * KISS24A  *(testing)*
+  * FURLING45MINI  *(hardware modification)*
 
   forked from: <br/>
   <https://github.com/conuthead/> <br/>
---
+++ src/main/target/RAZOR32V2/target.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// input ,Ok
+#define INPUT_GPIO      GPIOA
+#define INPUT_PIN       LL_GPIO_PIN_2
+
+// LEDs ,Ok
+#define LED_RED_GPIO    GPIOA
+#define LED_RED_PIN     LL_GPIO_PIN_15
+#define LED_GREEN_GPIO  GPIOB
+#define LED_GREEN_PIN   LL_GPIO_PIN_3
+#define LED_BLUE_GPIO   GPIOB
+#define LED_BLUE_PIN    LL_GPIO_PIN_4
+#define LED_MASK        LL_AHB1_GRP1_PERIPH_GPIOA | LL_AHB1_GRP1_PERIPH_GPIOB
